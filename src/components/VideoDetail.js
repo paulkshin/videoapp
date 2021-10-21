5b7fa98c14cf8cbd58237828ca9582325402d62a
Paul Shin <paulkisooshin@gmail.com>
2021-10-21 16:13:26 -0700

    finishing video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Loading...</div>;
+    return <div>Search something!</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
